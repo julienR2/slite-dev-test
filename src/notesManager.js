8a5b93c12e5f706b349351a47cd8ac6d33434165
julienr2 <julien.rougeron@gmail.com>
2018-11-16 02:00:06 -0800

    refactor to reuse more existing functions

--- src/notesManager.js
+++ src/notesManager.js
@@ -32,7 +32,7 @@ class NotesManager {
     ))
   }
 
-  get(docId, format = 'txt') {
+  getParsedData(docId) {
     return new Promise((resolve, reject) => fs.readFile(
       this.getPath(docId),
       (err, data) => {
@@ -42,12 +42,17 @@ class NotesManager {
 
         try {
           const parsedData = JSON.parse(data)
-          resolve(parsedData.content)
+          resolve(parsedData)
         } catch (e) {
           reject(e)
         }
       }
     ))
+  }
+
+  get(docId, format = 'txt') {
+    return this.getParsedData(docId)
+      .then(({ content }) => content)
   }
 
   delete(docId, format = 'txt') {
@@ -60,32 +65,21 @@ class NotesManager {
   insert(docId, position, text) {
     const path = this.getPath(docId)
 
-    return new Promise((resolve, reject) => fs.readFile(
-      path,
-      (err, data) => {
-        if (err) {
-          reject(err)
-        }
+    return this.getParsedData(docId)
+      .then((parsedData) => {
+        const currentText = parsedData.content
+        // absPosition is the position where to insert the new content or the last position before empty string
+        const absPosition = position || (currentText.length - EMPTY.length)
+        // Get content before and after position
+        const contentBefPos = currentText.substring(0, absPosition)
+        const contentAftPos = currentText.substring(absPosition)
 
-        try {
-          const parsedData = JSON.parse(data)
-          const currentText = parsedData.content
-          // absPosition is the position where to insert the new content or the last position before empty string
-          const absPosition = position || (currentText.length - EMPTY.length)
-          // Get content before and after position
-          const contentBefPos = currentText.substring(0, absPosition)
-          const contentAftPos = currentText.substring(absPosition)
+        parsedData.content = contentBefPos + text + contentAftPos
 
-          parsedData.content = contentBefPos + text + contentAftPos
-
-          this.create(docId, parsedData)
-            .then(resolve)
-            .catch(reject)
-        } catch (e) {
-          reject(e)
-        }
-      }
-    ))
+        return this.create(docId, parsedData)
+          .then(resolve)
+          .catch(reject)
+      })
   }
 }
 
